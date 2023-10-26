9028f1e4981153df0558127cf68d2c8625682220
abrahamgw14 <agarciawals@gmail.com>
2023-10-26 20:13:18 +0200

    feat: done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import React from "react";
+import ContactTable from "./components/contact-table/contact-table";
+import contacts from "./contacts.json";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <div>
+        <ContactTable />
+      </div>
     </div>
   );
 }
